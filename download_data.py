35a4e95422dacec9e8bd1af353faca151465ba0b
Jean-Marc Skopek <jskopek@gmail.com>
2014-11-23 18:58:21 -0800

    Added optional target path

--- download_data.py
+++ download_data.py
@@ -36,19 +36,20 @@ def download_file(url, file_name):
 
     f.close()
 
-def get_args(argv, source_url=None, header_url=None, dem_files=None):
+def get_args(argv, source_url=None, header_url=None, dem_files=None, target=None):
     """
     Parases any custom arguments from the command line, reverting to the the default if none passed
-    Returns a (source_url, header_url, dem_files) pair of arguments.
+    Returns a (source_url, header_url, dem_files, target) pair of arguments.
     """
 
     try:
-        options, args = getopt.getopt(argv, 's:h:d:', ['source-url=','header-url=','dem-files='])
+        options, args = getopt.getopt(argv, 's:h:d:t:', ['source-url=','header-url=','dem-files=','target='])
     except getopt.GetoptError:
-        print 'download_data.py -s <sourceurl> -h <headerurl> -d <demfiles>'
-        print '  -s, --source-url: The root URI folder that contains the dem files'
-        print '  -h, --header-url: The root URI folder that contains the dem files'
-        print '  -d, --dem-files:  A comman separated list of DEM files that we wish to download (e.g. a10g,f10h)'
+        print 'download_data.py -s <sourceurl> -h <headerurl> -d <demfiles> -t <target>'
+        print '  -s, --source-url:   The root URI folder that contains the dem files'
+        print '  -h, --header-url:   The root URI folder that contains the dem files'
+        print '  -d, --dem-files:    A comman separated list of DEM files that we wish to download (e.g. a10g,f10h)'
+        print '  -t, --target:       The target folder to download source and header files into'
         sys.exit(2)
 
     for opt, val in options:
@@ -58,10 +59,15 @@ def get_args(argv, source_url=None, header_url=None, dem_files=None):
             header_url = val
         elif opt in ['-d', '--dem-files']:
             dem_files = val
+        elif opt in ['-t', '--target']:
+            target = val
+            if not os.path.isdir(target):
+                print '--target path does not exist or is not a valid folder: %s' % target
+                sys.exit(2)
 
-    return (source_url, header_url, dem_files)
+    return (source_url, header_url, dem_files, target)
         
-def download_and_extract(source_url, header_url, dem_files):
+def download_and_extract(source_url, header_url, dem_files, target):
     """
     Download each dem file from the source and extract it
     """
@@ -70,25 +76,27 @@ def download_and_extract(source_url, header_url, dem_files):
         # download source file
         full_source_url = '%s%s.zip' % (source_url, dem_file)
         source_file_name = full_source_url.split('/')[-1]
-        download_file(full_source_url, source_file_name)
+        source_file_path = os.path.join(target, source_file_name)
+        download_file(full_source_url, source_file_path)
 
         # download header file
         full_header_url = '%s%s.hdr' % (header_url, dem_file)
         header_file_name = full_header_url.split('/')[-1]
-        download_file(full_header_url, header_file_name)
+        header_file_path = os.path.join(target, header_file_name)
+        download_file(full_header_url, header_file_path)
 
         # extract the source file
         try:
-            zf = zipfile.ZipFile(source_file_name, 'r')
+            zf = zipfile.ZipFile(source_file_path, 'r')
         except zipfile.BadZipfile:
             print 'Invalid zip file provided at: %s' % url
             sys.exit(2)
         else:
-            zf.extractall()
+            zf.extractall(target)
             zf.close()
 
         # remove the source file
-        os.remove(source_file_name)
+        os.remove(source_file_path)
 
 
 
@@ -96,7 +104,7 @@ if __name__ == '__main__':
     args = get_args(sys.argv[1:],
         source_url='http://www.ngdc.noaa.gov/mgg/topo/DATATILES/elev/',
         header_url='http://www.ngdc.noaa.gov/mgg/topo/elev/esri/hdr/',
-        dem_files='a10g'
+        dem_files='a10g',
+        target=''
     )
-    print args
     download_and_extract(*args)
